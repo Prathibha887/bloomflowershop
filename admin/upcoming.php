175319b474fe4bf2d52ba95a9b1949f02acfabd4
Prathibha S <prathibhas887@gmail.com>
2024-12-24 20:41:21 +0530

    Add files via upload

---
+++ admin/upcoming.php
@@ -0,0 +1,536 @@
+<?php
+session_start();
+if(isset($_SESSION['floweruser']))
+{
+	$floweruser=$_SESSION['floweruser'];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Basic Page Info -->
+	<meta charset="utf-8">
+	<title>DeskApp - Bootstrap Admin Dashboard HTML Template</title>
+
+	<!-- Site favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="vendors/images/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="vendors/images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="vendors/images/favicon-16x16.png">
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="vendors/styles/core.css">
+	<link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="vendors/styles/style.css">
+<style>
+.sidebar-menu .dropdown-toggle .icon-copy {
+    position: absolute;
+    left: 10px;
+    width: 42px;
+    height: 42px;
+    font-size: 24px;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    color: #fff;
+    background-color: transparent;
+    top: 50%;
+    text-align: center;
+    border-radius: 4px;
+    -webkit-transition: all .3s ease-in-out;
+    transition: all .3s ease-in-out;
+    -webkit-transform: translate(0,-50%);
+    transform: translate(0,-50%);
+}
+</style>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-119386393-1');
+	</script>
+</head>
+<body>
+	<div class="pre-loader">
+		<div class="pre-loader-box">
+			<div class="loader-logo"><img src="img/l.png" alt=""></div>
+			<div class='loader-progress' id="progress_div">
+				<div class='bar' id='bar1'></div>
+			</div>
+			<div class='percent' id='percent1'>0%</div>
+			<div class="loading-text">
+				Loading...
+			</div>
+		</div>
+	</div>
+
+	<div class="header">
+		<div class="header-left">
+			<div class="menu-icon dw dw-menu"></div>
+			<div class="search-toggle-icon dw dw-search2" data-toggle="header_search"></div>
+			
+		</div>
+		<div class="header-right">
+			
+			
+			<div class="user-info-dropdown">
+				<div class="dropdown">
+					<a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+						<span class="user-icon">
+							<img src="img/logo.png" alt=""  style="width:52px;height:52px;">
+						</span>
+						<span class="user-name">The Bloom Flower Shop Owner</span>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+						
+						<a class="dropdown-item" href="login.html"><i class="dw dw-logout"></i> Log Out</a>
+					</div>
+				</div>
+			</div>
+			<div class="github-link">
+				<a href="https://github.com/dropways/deskapp" target="_blank"><img src="vendors/images/github.svg" alt=""></a>
+			</div>
+		</div>
+	</div>
+
+	<div class="right-sidebar">
+		<div class="sidebar-title">
+			<h3 class="weight-600 font-16 text-blue">
+				Layout Settings
+				<span class="btn-block font-weight-400 font-12">User Interface Settings</span>
+			</h3>
+			<div class="close-sidebar" data-toggle="right-sidebar-close">
+				<i class="icon-copy ion-close-round"></i>
+			</div>
+		</div>
+		<div class="right-sidebar-body customscroll">
+			<div class="right-sidebar-body-content">
+				<h4 class="weight-600 font-18 pb-10">Header Background</h4>
+				<div class="sidebar-btn-group pb-30 mb-10">
+					<a href="javascript:void(0);" class="btn btn-outline-primary header-white active">White</a>
+					<a href="javascript:void(0);" class="btn btn-outline-primary header-dark">Dark</a>
+				</div>
+
+				<h4 class="weight-600 font-18 pb-10">Sidebar Background</h4>
+				<div class="sidebar-btn-group pb-30 mb-10">
+					<a href="javascript:void(0);" class="btn btn-outline-primary sidebar-light ">White</a>
+					<a href="javascript:void(0);" class="btn btn-outline-primary sidebar-dark active">Dark</a>
+				</div>
+
+				<h4 class="weight-600 font-18 pb-10">Menu Dropdown Icon</h4>
+				<div class="sidebar-radio-group pb-10 mb-10">
+					<div class="custom-control custom-radio custom-control-inline">
+						<input type="radio" id="sidebaricon-1" name="menu-dropdown-icon" class="custom-control-input" value="icon-style-1" checked="">
+						<label class="custom-control-label" for="sidebaricon-1"><i class="fa fa-angle-down"></i></label>
+					</div>
+					<div class="custom-control custom-radio custom-control-inline">
+						<input type="radio" id="sidebaricon-2" name="menu-dropdown-icon" class="custom-control-input" value="icon-style-2">
+						<label class="custom-control-label" for="sidebaricon-2"><i class="ion-plus-round"></i></label>
+					</div>
+					<div class="custom-control custom-radio custom-control-inline">
+						<input type="radio" id="sidebaricon-3" name="menu-dropdown-icon" class="custom-control-input" value="icon-style-3">
+						<label class="custom-control-label" for="sidebaricon-3"><i class="fa fa-angle-double-right"></i></label>
+					</div>
+				</div>
+
+				<h4 class="weight-600 font-18 pb-10">Menu List Icon</h4>
+				<div class="sidebar-radio-group pb-30 mb-10">
+					<div class="custom-control custom-radio custom-control-inline">
+						<input type="radio" id="sidebariconlist-1" name="menu-list-icon" class="custom-control-input" value="icon-list-style-1" checked="">
+						<label class="custom-control-label" for="sidebariconlist-1"><i class="ion-minus-round"></i></label>
+					</div>
+					<div class="custom-control custom-radio custom-control-inline">
+						<input type="radio" id="sidebariconlist-2" name="menu-list-icon" class="custom-control-input" value="icon-list-style-2">
+						<label class="custom-control-label" for="sidebariconlist-2"><i class="fa fa-circle-o" aria-hidden="true"></i></label>
+					</div>
+					<div class="custom-control custom-radio custom-control-inline">
+						<input type="radio" id="sidebariconlist-3" name="menu-list-icon" class="custom-control-input" value="icon-list-style-3">
+						<label class="custom-control-label" for="sidebariconlist-3"><i class="dw dw-check"></i></label>
+					</div>
+					<div class="custom-control custom-radio custom-control-inline">
+						<input type="radio" id="sidebariconlist-4" name="menu-list-icon" class="custom-control-input" value="icon-list-style-4" checked="">
+						<label class="custom-control-label" for="sidebariconlist-4"><i class="icon-copy dw dw-next-2"></i></label>
+					</div>
+					<div class="custom-control custom-radio custom-control-inline">
+						<input type="radio" id="sidebariconlist-5" name="menu-list-icon" class="custom-control-input" value="icon-list-style-5">
+						<label class="custom-control-label" for="sidebariconlist-5"><i class="dw dw-fast-forward-1"></i></label>
+					</div>
+					<div class="custom-control custom-radio custom-control-inline">
+						<input type="radio" id="sidebariconlist-6" name="menu-list-icon" class="custom-control-input" value="icon-list-style-6">
+						<label class="custom-control-label" for="sidebariconlist-6"><i class="dw dw-next"></i></label>
+					</div>
+				</div>
+
+				<div class="reset-options pt-30 text-center">
+					<button class="btn btn-danger" id="reset-settings">Reset Settings</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="home.php">
+				<img src="img/bloom.png" alt="" class="dark-logo" style="width:90px;height:65px;">
+				<img src="img/l.png" alt="" class="light-logo" style="width:900px;height:60px;">
+			</a>
+			<div class="close-sidebar" data-toggle="left-sidebar-close">
+				<i class="ion-close-round"></i>
+			</div>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					
+						<a href="home.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-house-1"></span><span class="mtext">Home</span>
+						</a>
+						<a href="upcoming.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-house-1"></span><span class="mtext">Upcoming</span>
+						</a>
+						
+					
+					
+						<a href="special.php" class="dropdown-toggle no-arrow">
+							<i class="icon-copy ion-ios-pricetag"></i><span class="mtext">Special Offers</span>
+						</a>
+						
+					
+					
+						<a href="footer.php" class="dropdown-toggle no-arrow">
+						<i class="icon-copy fi-camera" width=50px height=50px></i> <span class="mtext">Add Footer Images</span>
+						</a>
+						
+					<li>
+						<a href="gallery.php" class="dropdown-toggle no-arrow">
+							<i class="icon-copy ion-android-image"></i><span class="mtext">Add Gallery Images</span>
+						</a>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<i class="icon-copy ion-ios-flower-outline"></i><span class="mtext">The Bloom Flower Shop </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="mainservice.php">Main Service</a></li>
+							<li><a href="ouerservice.php">Our Service</a></li>
+							<li><a href="Varietyservice.php">Variety service</a></li>
+							
+						</ul>
+					</li>
+					
+						<a href="about.php" class="dropdown-toggle no-arrow">
+							<i class="icon-copy ion-android-mail"></i><span class="mtext">About Flowers<span>
+						</a>
+						
+					
+						<a href="products.php" class="dropdown-toggle no-arrow">
+							<i class="icon-copy ion-clipboard"></i><span class="mtext">Our Products</span>
+						</a>
+						
+					
+						<a href="view.php" class="dropdown-toggle no-arrow">
+							<i class="icon-copy ion-ios-search-strong"></i><span class="mtext">View Ordered Items</span>
+						</a>
+						
+					
+						<a href="cart.php" class="dropdown-toggle no-arrow">
+							<i class="icon-copy ion-android-cart"></i><span class="mtext">View Cart Product</span>
+						</a>
+						
+
+					<!--<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-copy"></span><span class="mtext">External Billing</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="blank.html">Blank</a></li>
+							<li><a href="contact-directory.html">Contact Directory</a></li>
+							<li><a href="blog.html">Blog</a></li>
+							<li><a href="blog-detail.html">Blog Detail</a></li>
+							<li><a href="product.html">Product</a></li>
+							<li><a href="product-detail.html">Product Detail</a></li>
+							<li><a href="faq.html">FAQ</a></li>
+							<li><a href="profile.html">Profile</a></li>
+							<li><a href="gallery.html">Gallery</a></li>
+							<li><a href="pricing-table.html">Pricing Tables</a></li>
+						</ul>
+					</li>-->
+					
+					
+						<a href="feedback.php" class="dropdown-toggle no-arrow">
+							<i class="icon-copy ion-social-whatsapp"></i><span class="mtext">Feedback</span>
+						</a>
+						<a href="logout.php" class="dropdown-toggle no-arrow">
+						<i class="icon-copy fa fa-sign-out" aria-hidden="true"></i><span class="mtext">Logout</span>
+						</a>
+					
+					
+					
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="mobile-menu-overlay"></div>
+
+	<div class="main-container">
+		<div class="pd-ltr-20 xs-pd-20-10">
+			<div class="min-height-200px">
+				<div class="page-header">
+					<div class="row">
+						<div class="col-md-6 col-sm-12">
+							<div class="title">
+								<h4>Add our Products</h4>
+							</div>
+							<nav aria-label="breadcrumb" role="navigation">
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+									<li class="breadcrumb-item active" aria-current="page">Product details</li>
+								</ol>
+							</nav>
+						</div>
+						
+					</div>
+				</div>
+				<!-- Default Basic Forms Start -->
+				<div class="pd-20 card-box mb-30">
+					<div class="clearfix">
+						<div class="pull-left">
+							<h4 class="text-blue h4">Our Product</h4>
+							
+						</div>
+						
+					</div>
+					<form method="post" action="#" enctype="multipart/form-data">
+						<div class="form-group row">
+						
+							<label class="col-sm-12 col-md-2 col-form-label">Add Images</label>
+							<div class="col-sm-12 col-md-10">
+								<input type="file"  name="image" class="form-control-file form-control height-auto">
+							</div>
+							
+						</div>
+						<div class="form-group row">
+						
+							<label class="col-sm-12 col-md-2 col-form-label">Product Name</label>
+							<div class="col-sm-12 col-md-10">
+								<input type="text"  name="product" class="form-control-file form-control height-auto" placeholder="Product">
+							</div>
+							
+						</div>
+						<div class="form-group row">
+						
+							<label class="col-sm-12 col-md-2 col-form-label">Event Name</label>
+							<div class="col-sm-12 col-md-10">
+								<input type="text"  name="event" class="form-control-file form-control height-auto" placeholder="Product">
+							</div>
+							
+						</div>
+						<div class="form-group row">
+						
+							<label class="col-sm-12 col-md-2 col-form-label">Rate:</label>
+							<div class="col-sm-12 col-md-10">
+								<input type="text"  name="rate" class="form-control-file form-control height-auto" >
+							</div>
+							
+						</div>
+					
+						<button type="submit" name="submit" value="Submit" class="btn btn-outline-primary" style="margin-left:180px;">Submit</button>
+					</form>
+					<?php					 						 
+if(isset($_POST['submit']))
+{
+	error_reporting(1);
+	include("config.php");
+	   $name=$_POST['name'];
+		$product=$_POST['product'];
+		$rate=$_POST['rate'];
+		$event=$_POST['event'];
+
+		$fname = $_FILES["image"]["name"];
+		$filename = $name.$fname;
+		$tempname = $_FILES["image"]["tmp_name"];   
+        $folder = "upcoming/".$filename;
+		
+        if (move_uploaded_file($tempname, $folder))  
+		{
+			$query ="INSERT INTO upcoming(image,product,rate,event) VALUES('".$filename."','".$product."','".$rate."','".$event."')";
+            mysqli_query($con, $query);
+			echo "<script>
+					alert(' uploaded Successfully');
+				</script>";
+			echo "<script> location.href='upcoming.php'; </script>";
+        }else{
+            echo "<script>
+					alert('Upload Failed,IMAGE should be less than 2GB');
+				</script>";
+		}
+}
+?>
+				</div>
+				<!-- Default Basic Forms End -->
+
+<div class="pd-20 card-box mb-30">
+					<div class="clearfix mb-20">
+						<div class="pull-left">
+							<h4 class="text-blue h4">View Our Products </h4>
+							
+						</div>
+						
+					</div>
+					<table class="table " style="border-top:0;">
+						<thead>
+							<tr>
+								<th scope="col">Sl.no</th>
+								
+								<th scope="col">Image</th>
+								<th scope="col">Product Name</th>
+								<th scope="col">Event</th>
+								<th scope="col">Rate</th>
+								
+								<th scope="col">Delete</th>
+								
+							</tr>
+						</thead>
+						<tbody>
+						<?php
+
+include 'config.php';
+
+$sql1 = "select * from upcoming";
+
+$result1 = mysqli_query($con,$sql1);
+
+$num1=mysqlI_num_rows($result1);
+
+$sl=0;
+
+if($num1 > 0)
+
+{
+
+while($row1 = mysqli_fetch_array($result1))
+
+{
+
+$sl+=1;
+
+$id=$row1[0];
+$product=$row1['product'];
+$rate=$row1['rate'];
+$event=$row1['event'];
+
+$image=$row1['image'];
+
+
+
+?>
+
+							<tr>
+								<th scope="row"><?php echo $sl; ?></th>
+								
+								<td><img src="upcoming/<?php echo $image; ?>"  style="width:130px;height:110px;"></td>
+								<th scope="col"><?php echo $product; ?></th>
+								<th scope="col"><?php echo $event; ?></th>
+								<th scope="col"><?php echo $rate; ?></th>
+								
+								<td>
+								<form action="#" method="post">
+
+<input type="text" name="id[]" value="<?php echo $id; ?>" hidden="true">
+								<button type="submit" name="delete" class="btn btn-outline-danger">Delete</button>
+								</form>
+								</td>
+							</tr>
+			
+<?php
+
+}
+
+}
+
+?>
+							
+						</tbody>
+					</table>
+					<?php
+
+if(isset($_POST['delete']))
+
+{
+
+foreach ($_POST['id'] as $key => $value)
+
+{
+
+$selectano=$value;
+
+}
+
+$query1 = "delete from upcoming where id='".$selectano."'";
+
+mysqli_query($con,$query1) or die(mysqli_error($con));
+
+echo "<script> location.href='upcoming.php'; </script>";
+
+}
+
+?>
+					<div class="collapse collapse-box" id="striped-table">
+						<div class="code-box">
+							<div class="clearfix">
+								<a href="javascript:;" class="btn btn-primary btn-sm code-copy pull-left"  data-clipboard-target="#striped-table-code"><i class="fa fa-clipboard"></i> Copy Code</a>
+								<a href="#striped-table" class="btn btn-primary btn-sm pull-right" rel="content-y"  data-toggle="collapse" role="button"><i class="fa fa-eye-slash"></i> Hide Code</a>
+							</div>
+							<pre><code class="xml copy-pre" id="striped-table-code">
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+    </tr>
+  </tbody>
+</table>
+							</code></pre>
+						</div>
+					</div>
+				</div>
+				<!-- Striped table End -->
+				<!-- horizontal Basic Forms Start -->
+				
+
+			</div>
+			
+		</div>
+	</div>
+	<!-- js -->
+	<script src="vendors/scripts/core.js"></script>
+	<script src="vendors/scripts/script.min.js"></script>
+	<script src="vendors/scripts/process.js"></script>
+	<script src="vendors/scripts/layout-settings.js"></script>
+</body>
+</html>
+<?php
+}
+else
+{
+	echo"<script>location.href='index.php';</script>";
+}
+?>
